1ddf7e3d728a0b863873c577e1bd3588450c9c19
marco-attica <marco.galbarini@atticalabs.com>
2021-03-29 17:10:14 -0300

    Style list of items and add close button

--- src/components/Tasks/List.js
+++ src/components/Tasks/List.js
@@ -5,16 +5,23 @@ const List = props => {
     console.log("✌: ", props.items);
     return (
         <section className="tasks-list">
-            {props.items.map((item) => {
-                // console.log('🚀', item);
-                // console.log('👌', index);
-                return (
-                    <React.Fragment>
-                        <span>{item.text}</span>
-                        <span>{item.total}</span><br />
-                    </React.Fragment>
-                )
-            })}
+            <ul className="list-group">
+                {props.items.map((item, i) => {
+                    // console.log('🚀', item);
+                    // console.log('👌', index);
+                    return (
+                        <React.Fragment>
+                            <li key={i} className="list-group-item d-flex align-items-center" >
+                                <span className="mr-auto">{item.text}</span>
+                                <span className="badge badge-primary badge-pill text-white flex-end">{item.total}</span>
+                                <button type="button" className="close ml-3" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </li>
+                        </React.Fragment>
+                    )
+                })}
+            </ul>
         </section >
     );
 };
